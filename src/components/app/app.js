5254a875c13a4fb0c95c83297c0310a534d5ec35
Your Name <3767082@mail.ru>
2020-05-29 02:47:37 +0300

    added hooks and logick undefind array

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,50 +1,64 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import './app.scss'
 
-export default class App extends Component {
-
-    state = {
-        todoData: [
-            {label:"is", value:true, id:1 },
-            {label:"is not", value:false, id:2 },
-        ]
+export default function App() {
+    let array;
+    const isDataTrue = (array) => {
+        if (array) {
+            return (
+                array
+            );
+        } else {
+            return ([
+                {label: "your data", value: true, id: 1},
+                {label: "your data not", value: false, id: 2},
+            ]);
+        }
     };
+
+    // here some JSON.parse();
+    array = [
+        {label: "is", value: true, id: 1},
+        {label: "is not", value: false, id: 2},
+    ];
+
+    // uncomment if array not found (undefined);
+    array = undefined;
+
+    const [data, setData] = useState(isDataTrue(array));
 
     //I would like to take labels and inputs into separate components.
     // But the technical task requires one component.
+    return (
+        <div className="app">
+            <form action="" className="app__form">
+                <input
+                    type="radio"
+                    id={data[0].id}
+                    name="form"
+                    value={data[0].value}
+                    checked="true"
+                />
+                <label
+                    className="app__form--button-green"
+                    htmlFor={data[0].id}>
+                    {data[0].label}
+                </label>
+                <input
+                    type="radio"
+                    id={data[1].id}
+                    name="form"
+                    value={data[1].value}
+                />
+                <label
+                    className="app__form--button-pink"
+                    htmlFor={data[1].id}>
+                    {data[1].label}
+                </label>
+            </form>
+        </div>
+    );
+};
 
-    render() {
-        const {todoData} = this.state;
-        return (
-            <div className="app">
-                <form action="" className="app__form">
-                    <input
-                        type="radio"
-                        id={todoData[0].id}
-                        name="form"
-                        value={todoData[0].value}
-                        checked="true"
-                    />
-                    <label
-                        className="app__form--button-green"
-                        htmlFor={todoData[0].id}>
-                        {todoData[0].label}
-                    </label>
-                    <input
-                        type="radio"
-                        id={todoData[1].id}
-                        name="form"
-                        value={todoData[1].value}
-                    />
-                    <label
-                        className="app__form--button-pink"
-                        htmlFor={todoData[1].id}>
-                        {todoData[1].label}
-                    </label>
-                </form>
-            </div>
-        );
-    }
-}
 
 
